8bc669b36ddda41144059dbd4c586cd047782b4f
Olivier <ducrouxolivier@gmail.com>
2025-05-08 12:20:58 +0200

    sanitize from and to email

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,2 +1,10 @@
 class Email < ApplicationRecord
+  before_validation :sanitize_from_and_to
+
+  private
+
+  def sanitize_from_and_to
+    self.from = Array(self.from).first if self.from.is_a?(Array)
+    self.to = Array(self.to).first if self.to.is_a?(Array)
+  end
 end
