5579e4a8764d876959a3c3e5b713b502501f0ec6
Olivier <ducrouxolivier@gmail.com>
2025-05-08 12:32:51 +0200

    fix

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,10 +1,16 @@
+require 'json'
+
 class Email < ApplicationRecord
   before_validation :sanitize_from_and_to
 
   private
 
   def sanitize_from_and_to
-    self.from = Array(self.from).first if self.from.is_a?(Array)
-    self.to = Array(self.to).first if self.to.is_a?(Array)
+    self.from = parse_email(self.from)
+    self.to = parse_email(self.to)
+  end
+
+  def parse_email(value)
+    JSON.parse(value).first
   end
 end
