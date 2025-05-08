de5b35be6806f6aeb3c73142e7d8f7c6563f6507
Olivier <ducrouxolivier@gmail.com>
2025-05-08 12:04:25 +0200

    remove user reference from emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
-  has_many :emails, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 
